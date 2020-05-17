a7e5810e5813a8d7baadb970c0acdbd352d7d5b6
[TD-Engineering] Yuda Prama <yudhaprama@icloud.com>
2020-05-18 05:35:27 +0700

    Revert "move to youtube.go"
    This reverts commit 8b9028a5

--- youtube.go
+++ youtube.go
@@ -20,38 +20,6 @@ type Youtube struct {
 	videoInfo string
 }
 
-func GetVideoInfo(url string) (Videos, error) {
-	y := NewYoutube()
-	if err := y.DecodeURL(url); err != nil {
-		return nil, err
-	}
-	return y.Videos, nil
-}
-
-func GetVideoInfoAsByte(url string) ([]byte, error) {
-	info, err := GetVideoInfo(url)
-	if err != nil {
-		return nil, err
-	}
-	return info.Marshal()
-}
-
-func GetVideoInfoAsJSON(url string) (string, error) {
-	asByte, err := GetVideoInfoAsByte(url)
-	if err != nil {
-		return "", err
-	}
-	return string(asByte), nil
-}
-
-func GetVideoInfoAsJSONWithoutErr(url string) string {
-	str, err := GetVideoInfoAsJSON(url)
-	if err != nil {
-		return ""
-	}
-	return str
-}
-
 // NewYoutube will initialize youtube object
 func NewYoutube() *Youtube {
 	return &Youtube{}
