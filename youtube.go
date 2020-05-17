8b9028a53df72c21e6f0ab711e1b93525ba7324b
[TD-Engineering] Yuda Prama <yudhaprama@icloud.com>
2020-05-18 02:54:44 +0700

    move to youtube.go

--- youtube.go
+++ youtube.go
@@ -15,9 +15,41 @@ import (
 
 // Youtube is object for youtube
 type Youtube struct {
-	Videos            Videos
-	VideoID           string
-	videoInfo         string
+	Videos    Videos
+	VideoID   string
+	videoInfo string
+}
+
+func GetVideoInfo(url string) (Videos, error) {
+	y := NewYoutube()
+	if err := y.DecodeURL(url); err != nil {
+		return nil, err
+	}
+	return y.Videos, nil
+}
+
+func GetVideoInfoAsByte(url string) ([]byte, error) {
+	info, err := GetVideoInfo(url)
+	if err != nil {
+		return nil, err
+	}
+	return info.Marshal()
+}
+
+func GetVideoInfoAsJSON(url string) (string, error) {
+	asByte, err := GetVideoInfoAsByte(url)
+	if err != nil {
+		return "", err
+	}
+	return string(asByte), nil
+}
+
+func GetVideoInfoAsJSONWithoutErr(url string) string {
+	str, err := GetVideoInfoAsJSON(url)
+	if err != nil {
+		return ""
+	}
+	return str
 }
 
 // NewYoutube will initialize youtube object
